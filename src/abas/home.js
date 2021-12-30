34cf3a14b33162053025656d27d5cb400f677af3
Isabella Oliveira <goliveirapk@gmail.com>
2021-12-30 16:35:17 -0300

    salvando componentes como extenções

--- src/abas/home.js
+++ src/abas/home.js
@@ -1,19 +1,21 @@
-import React from "react";
+import React,{ Component } from "react";
 import "../css/projetosAbertos.css";
-import ListaProjetos from './ListaProjetos'
-function Home() {
-  return (
-    <div>
-      <ul className="projetosMenu">
-        <li className="botaoClaro">
-          <a href="/">Projetos Abertos</a>
-        </li>
-        <li className="botaoEscuro">
-          <a href="/Criar">Criar Projetos</a>
-        </li>
-      </ul>
-      <ListaProjetos />
-    </div>
-  );
+import ListaProjetos from "./ListaProjetos";
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <ul className="projetosMenu">
+          <li className="botaoClaro">
+            <a href="/">Projetos Abertos</a>
+          </li>
+          <li className="botaoEscuro">
+            <a href="/Criar">Criar Projetos</a>
+          </li>
+        </ul>
+        <ListaProjetos />
+      </div>
+    );
+  }
 }
 export default Home;
