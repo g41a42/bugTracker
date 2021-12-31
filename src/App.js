eafc1f09e40b53bd9b47a4adff62261997a64118
Isabella Oliveira <goliveirapk@gmail.com>
2021-12-31 12:30:52 -0300

    criação da aba projetos

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./css/reset.css";
 import Home from "./abas/home.js";
 import Cria from "./abas/CriaProjetos.js";
 import Nav from "./abas/nav.js";
+import Projetos from "./abas/Projetos";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { GlobalProvider } from "./context/GlobalState";
 
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route exect path="/" element={<Home />} />
           <Route path="/Criar" element={<Cria />} />
+          <Route path="/Projetos" element={<Projetos />} />
         </Routes>
       </Router>
     </GlobalProvider>
---
+++ src/abas/Projetos.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import Envolvidos from "./Envolvidos";
+import "../css/Projetos.css";
+class Projetos extends Component {
+  render() {
+    return (
+      <div className="all">
+        <ul className="projetosMenu">
+          <li className="botaoClaro">
+            <Link to="/Projetos">
+              <p>Titulo do Projeto</p>
+            </Link>
+          </li>
+        </ul>
+        <div className="container">
+          <div className="sobreOProjeto">
+            <p className="descProjeto">Descrição do Projeto</p>
+            <div className="bug">
+              <h2 className="bugTitulo"> Titulo do Bug ou Feature</h2>
+              <div className="bugInfo">
+                <h3 className="bugDescPH">Descrição</h3>
+                <p className="bugDesc">descrição do bug ou feature</p>
+                <h4>criação:</h4>
+                <p>data</p>
+                <h4>Prazo:</h4>
+                <p>data</p>
+              </div>
+                <div className="comentarios">
+                  <h3 className="comentariosPH">Comentários</h3>
+                  <ul>
+                    <li className="comentario">
+                      <h4>Nome</h4>
+                      <p>Comentario</p>
+                      <button>Responder</button>
+                    </li>
+                  </ul>
+                  <form className="comentar">
+                      <textarea placeholder="comentar bug"></textarea>
+                      <button>Comentar</button>
+                  </form>
+                </div>
+            </div>
+          </div>
+          <div className="alteraProjeto">
+            <Envolvidos />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default Projetos;
